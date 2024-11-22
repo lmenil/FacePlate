09cf7ba36f0d173652ec8d8d0e72cd9ee9df36c0
lmenil <lmenil@my.centennialcollege.ca>
2024-11-22 02:34:54 -0500

    2nd commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -20,7 +20,8 @@ export default defineConfig({
     build: {
         manifest: true,
         rollupOptions: {
-            input: "./src/main.jsx",
+            input: './index.html',
         },
     },
+    base: '/',
 });

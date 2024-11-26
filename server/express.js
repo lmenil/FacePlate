565ffb18cbd719535c662df5903cedf884db1462
lmenil <lmenil@my.centennialcollege.ca>
2024-11-26 12:20:06 -0500

    first commit

---
+++ server/express.js
@@ -0,0 +1,45 @@
+import express from 'express' 
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+import Template from '../server/template.js'
+import userRoutes from './Routes/user.routes.js'
+import authRoutes from './Routes/auth.routes.js'
+import recipeRoutes from './Routes/recipe.routes.js'
+import contactRoutes from './Routes/contact.routes.js'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+    const app = express()
+    const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+   app.use(express.json());
+   app.use(express.urlencoded({ extended: true }));
+   app.use('/', userRoutes)
+   app.use('/', authRoutes)
+   app.use('/', recipeRoutes)
+   app.use('/', contactRoutes)
+   app.use(bodyParser.json())
+   app.use(bodyParser.urlencoded({ extended: true }))
+   app.use(cookieParser())
+   app.use(compress())
+   app.use(helmet())
+   app.use(cors())
+//    app.get('/', (req, res) => {
+//     res.status(200).send(Template()) 
+//     })  
+    app.use((err, req, res, next) => {
+        if (err.name === 'UnauthorizedError') {
+        res.status(401).json({"error" : err.name + ": " + err.message}) 
+        }else if (err) {
+        res.status(400).json({"error" : err.name + ": " + err.message}) 
+        console.log(err)
+        } 
+        })
+
+    app.use(express.static(path.join(__dirname, "../dist/app")));
+          
+   export default app

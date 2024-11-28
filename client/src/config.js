b62f7e7b875fdee437aba9df8346f1a0e73cc9e9
lmenil <1ns0n978@gmail.com>
2024-11-28 06:57:35 -0500

    2nd commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
 //export const API_URL = 'http://localhost:3000';
-export const API_URL = import.meta.env.VITE_API_URL || 'https://faceplate-server.onrender.com';
+//export const API_URL = import.meta.env.VITE_API_URL || 'https://faceplate-server.onrender.com';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://faceplate-server.onrender.com';

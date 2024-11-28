1a02e42753a0c50d794f96d458766416358ed9e9
lmenil <1ns0n978@gmail.com>
2024-11-28 08:15:41 -0500

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv';
+dotenv.config({path: '../server/.env'}); 
 // export const API_URL = 'http://localhost:3000';
 //export const API_URL = 'https://faceplate-server.onrender.com';
 export const API_URL = import.meta.env.VITE_API_URL || 'https://faceplate-server.onrender.com';

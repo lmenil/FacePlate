b59aedb0dda79bcb6acd55bde3a32b0c18d312aa
lmenil <1ns0n978@gmail.com>
2024-11-28 20:16:19 -0500

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://faceplate-server.onrender.com';
 //export const API_URL = 'https://faceplate-server.onrender.com';
 //export const API_URL = import.meta.env.VITE_API_URL || 'https://faceplate-server.onrender.com';
 //export const API_URL = process.env.REACT_APP_API_URL || 'https://faceplate-server.onrender.com';
